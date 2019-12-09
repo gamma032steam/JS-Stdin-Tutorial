2599b084ec0cf9bba7199feea3f55e7dc025e31f
Matthew Lui <gamma032@gmail.com>
2019-12-09 17:58:06 +1100

    Fixed even generator bug

--- evengenerator.js
+++ evengenerator.js
@@ -1,30 +1,17 @@
-const readline = require('readline');
+const readline = require('readline-sync');
 
-var r1 = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-function ask(query) {
-    return new Promise(function(resolve) {
-        // The promise is fulfilled when the question is answered
-        r1.question(query, function(ans) {resolve(ans);});
-    })
-}
-
-async function main() {
+function main() {
     count = 0;
     while(true) {
         count += 2;
         console.log(count);
+        // If a multiple of ten
         if (count % 10 == 0) {
-            const answer = await ask('Continue? Y or N: ');
-                if (answer == 'N') {
-                    // We're not reading any more input
-                    r1.close();
-                    // Stop generating numbers
-                    break;
-                }
+            answer = readline.question('Continue? Y or N: ');
+            if (answer == 'N') {
+                // Stop generating numbers
+                break;
+            }
         }
     }
 }
